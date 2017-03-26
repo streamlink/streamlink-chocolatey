55d5b13a5e6a8776231484074661e5862c9b79b6
Scott Walters <me@scowalt.com>
2017-03-26 11:06:59 -0700

    v0.4.0

--- Streamlink/tools/chocolateyinstall.ps1
+++ Streamlink/tools/chocolateyinstall.ps1
@@ -13,7 +13,7 @@ $packageArgs = @{
 
   softwareName  = 'Streamlink*'
 
-  checksum      = '65DE0C28D508B960257FF30C759BC86BAF8A4A85017A6D8FCB6A08A8CF3F5629'
+  checksum      = 'F647CCBFAA712A13D5D41743A7FD69B9B93BD9451655BA943B57B4B768A1BC95'
   checksumType  = 'sha256'
 
   silentArgs   = '/S'
