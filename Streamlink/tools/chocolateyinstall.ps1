8535b121e71b0d796bfb4ac609fbbf78a6e3577f
Scott Walters <me@scowalt.com>
2022-02-27 19:30:51 -0800

    Added error for old install

--- Streamlink/tools/chocolateyinstall.ps1
+++ Streamlink/tools/chocolateyinstall.ps1
@@ -1,6 +1,10 @@
 ﻿
 $ErrorActionPreference = 'Stop';
 
+If ([Environment]::OSVersion.version.major -lt '10') {
+    Throw "Minimum required OS version is Windows 10"
+}
+
 $packageName= 'Streamlink'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = "https://github.com/streamlink/streamlink/releases/download/$env:chocolateyPackageVersion/streamlink-$env:chocolateyPackageVersion.exe"
