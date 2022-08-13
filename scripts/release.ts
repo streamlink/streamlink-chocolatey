1e1f33754af3dadd4f312d42ef9662319722f3e1
Scott Walters <me@scowalt.com>
2022-08-13 15:44:16 -0700

    Ported "new version available"

--- scripts/release.ts
+++ scripts/release.ts
@@ -1,16 +1,48 @@
-import path from 'path';
-import fs from 'fs';
-import {XMLParser} from 'fast-xml-parser'
+import path from "path";
+import fs from "fs";
+import { XMLParser } from "fast-xml-parser";
+import { Octokit } from "@octokit/rest";
+import { SemVer } from "semver";
 
-const parser = new XMLParser();
+const owner = "streamlink";
+const repo = "windows-builds";
+const nuspecPath = path.join(
+    __dirname,
+    "..",
+    "Streamlink",
+    "streamlink.nuspec"
+);
 
-const nuspec_path = path.join(__dirname, '..', 'Streamlink', 'streamlink.nuspec');
+async function main(): Promise<void> {
+    const current = getCurrentVersion();
+    const latest = await getLatestVersion();
+    if (current < latest) {
+        console.log(`New version available!`);
+    }
+}
 
-// synchronously read the file contents
-const nuspec_string = fs.readFileSync(nuspec_path, 'utf8');
-const nuspec = parser.parse(nuspec_string);
-const current_version = nuspec.package.metadata.version;
-console.log(`Current version: ${current_version}`);
+function getCurrentVersion(): SemVer {
+    const parser = new XMLParser();
 
+    // synchronously read the file contents
+    const nuspecString = fs.readFileSync(nuspecPath, "utf8");
+    const nuspec = parser.parse(nuspecString);
+    const currentVersion = nuspec.package.metadata.version;
+    console.log(`Current version: ${currentVersion}`);
 
-const release_repo = 'streamlink/windows-builds';
+    return new SemVer(currentVersion);
+}
+
+async function getLatestVersion() {
+    const octokit = new Octokit({});
+    const release = await octokit.repos.getLatestRelease({
+        owner,
+        repo,
+    });
+
+    const latestVersion = release.data.tag_name;
+    console.log(`Latest version: ${latestVersion}`);
+    return new SemVer(latestVersion);
+}
+
+main();
