011965995aca08e7fef100ef62cb156dbdfc3d53
scowalt <scowalt@users.noreply.github.com>
2024-01-20 15:30:07 +0000

    Automatic version update

--- Streamlink/tools/chocolateyinstall.ps1
+++ Streamlink/tools/chocolateyinstall.ps1
@@ -6,8 +6,8 @@ If ([Environment]::OSVersion.version.major -lt '10') {
 
 $packageName = 'Streamlink'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = "https://github.com/streamlink/windows-builds/releases/download/6.5.0-2/streamlink-6.5.0-2-py311-x86_64.exe"
-$hash = "81211ea26dab8dca83401d5e5e4d38b52fe47c02328f016ff8b0b635c53f3c1b"
+$url = "https://github.com/streamlink/windows-builds/releases/download/6.5.1-1/streamlink-6.5.1-1-py312-x86_64.exe"
+$hash = "9bf242c5ebbf1dd73b9ca5847812124892de64546042bc540ef3e31d3be781dc"
 
 $packageArgs = @{
     packageName    = $packageName
